aa006e68b838c4f5d6243515a36dca0bfe2858ae
Ben Oztalay <boztalay@gmail.com>
2020-03-21 19:44:16 -0700

    Fill in the actions some more

--- HandAndFoot.playground/Contents.swift
+++ HandAndFoot.playground/Contents.swift
@@ -1,9 +1,12 @@
-enum IllegalMoveError: Error {
+enum IllegalActionError: Error {
     case cardDoesntMatchBookRank
     case notEnoughCardsToStartBook
     case cannotStartBookWithGivenCards
     case tooManyWildsInBookToAddAnother
     case initialHandOrFootNotSizedCorrectly
+    case deckIsEmpty
+    case discardPileIsEmpty
+    case notEnoughPointsToLayDown
 }
 
 enum IllegalSetupError: Error {
@@ -33,6 +36,23 @@ enum CardRank: CaseIterable {
     case king
     case ace
     case joker
+}
+
+enum Round: Int {
+    case ninety = 90
+    case oneTwenty = 120
+    case oneFifty = 150
+    case oneEighty = 180
+}
+
+enum Action {
+    case drawFromDeck(Player)
+    case drawFromDiscardAndAddToBook(Player)
+    case drawFromDiscardAndCreateBook(Player, [Card])
+    case discardCard(Player, Card)
+    case layDownInitialBooks(Player, [[Card]])
+    case startBook(Player, [Card])
+    case addCardFromHandToBook(Player, Card)
 }
 
 struct Card {
@@ -66,12 +86,21 @@ class Deck {
         self.cards.append(Card(suit: .spades, rank: .joker))
     }
     
+    var isEmpty: Bool {
+        return (self.cards.count == 0)
+    }
+    
     func shuffle() {
         self.cards.shuffle()
     }
     
     func draw() -> Card? {
         return self.cards.popLast()
+    }
+    
+    func replenishCardsAndShuffle(cards: [Card]) {
+        self.cards = cards
+        self.shuffle()
     }
 }
 
@@ -81,11 +110,11 @@ class Book {
     
     init(initialCards: [Card]) throws {
         if initialCards.count < 3 {
-            throw IllegalMoveError.notEnoughCardsToStartBook
+            throw IllegalActionError.notEnoughCardsToStartBook
         }
         
         if initialCards.filter({ $0.canStartBook }).count == 0 {
-            throw IllegalMoveError.cannotStartBookWithGivenCards
+            throw IllegalActionError.cannotStartBookWithGivenCards
         }
         
         var bookRank: CardRank? = nil
@@ -119,6 +148,11 @@ class Book {
         return (self.cards.count == 7)
     }
     
+    var pointValue: Int {
+        fatalError("not implemented")
+        return 0
+    }
+    
     func addCard(card: Card) throws {
         if card.isWild {
             try self.addWildCard(card: card)
@@ -129,7 +163,7 @@ class Book {
     
     private func addWildCard(card: Card) throws {
         guard self.wildCount < (self.naturalCount - 1) else {
-            throw IllegalMoveError.tooManyWildsInBookToAddAnother
+            throw IllegalActionError.tooManyWildsInBookToAddAnother
         }
         
         self.cards.append(card)
@@ -137,7 +171,7 @@ class Book {
     
     private func addNaturalCard(card: Card) throws {
         guard card.rank == self.rank else {
-            throw IllegalMoveError.cardDoesntMatchBookRank
+            throw IllegalActionError.cardDoesntMatchBookRank
         }
         
         self.cards.append(card)
@@ -153,7 +187,7 @@ class Player {
     
     init(name: String, hand: [Card], foot: [Card]) throws {
         guard hand.count == 13 && foot.count == 13 else {
-            throw IllegalMoveError.initialHandOrFootNotSizedCorrectly
+            throw IllegalActionError.initialHandOrFootNotSizedCorrectly
         }
         
         self.name = name
@@ -161,11 +195,30 @@ class Player {
         self.foot = foot
         self.books = []
     }
+    
+    func addCardToHand(_ card: Card) {
+        self.hand.append(card)
+    }
+    
+    func addCardToBook(_ card: Card) throws {
+        fatalError("not implemented")
+    }
+    
+    func createBook(with cards: [Card]) throws {
+        fatalError("not implemented")
+    }
+    
+    func removeCardFromHand(_ card: Card) throws {
+        fatalError("not implemented")
+    }
 }
 
 class Game {
     private var deck: Deck
+    private var discards: [Card]
     private var players: [Player]
+    private var round: Round
+    private var actions: [Action]
     
     init(playerNames: [String]) throws {
         guard playerNames.count >= 2 else {
@@ -180,7 +233,11 @@ class Game {
         self.deck = Deck(standardDeckCount: deckCount)
         self.deck.shuffle()
         
+        self.discards = []
         self.players = []
+        self.round = .ninety
+        self.actions = []
+        
         for playerName in playerNames {
             let player = try self.setUpPlayer(name: playerName)
             self.players.append(player)
@@ -200,5 +257,81 @@ class Game {
         }
         
         return try Player(name: name, hand: hand, foot: foot)
+    }
+    
+    func apply(action: Action) throws {
+        switch (action) {
+            case let .drawFromDeck(player):
+                self.applyDrawFromDeckAction(player: player)
+            case let .drawFromDiscardAndAddToBook(player):
+                try self.applyDrawFromDiscardAndAddToBookAction(player: player)
+            case let .drawFromDiscardAndCreateBook(player, cards):
+                try self.applyDrawFromDiscardAndCreateBookAction(player: player, cards: cards)
+            case let .discardCard(player, card):
+                try self.applyDiscardCardAction(player: player, card: card)
+            case let .layDownInitialBooks(player, cards):
+                try self.applyLayDownInitialBooksAction(player: player, cards: cards)
+            case let .startBook(player, cards):
+                try self.applyStartBookAction(player: player, cards: cards)
+            case let .addCardFromHandToBook(player, card):
+                try self.applyAddCardFromHandToBookAction(player: player, card: card)
+        }
+    }
+    
+    func applyDrawFromDeckAction(player: Player) {
+        player.addCardToHand(self.deck.draw()!)
+
+        if self.deck.isEmpty {
+            self.deck.replenishCardsAndShuffle(cards: self.discards)
+            self.discards = []
+        }
+    }
+    
+    func applyDrawFromDiscardAndAddToBookAction(player: Player) throws {
+        guard self.discards.count > 0 else {
+            throw IllegalActionError.discardPileIsEmpty
+        }
+
+        let card = self.discards.popLast()!
+        try player.addCardToBook(card)
+    }
+    
+    func applyDrawFromDiscardAndCreateBookAction(player: Player, cards: [Card]) throws {
+        guard self.discards.count > 0 else {
+            throw IllegalActionError.discardPileIsEmpty
+        }
+        
+        let card = self.discards.popLast()!
+        let cardsInBook = cards + [card]
+
+        player.addCardToHand(card)
+        try player.createBook(with: cardsInBook)
+    }
+    
+    func applyDiscardCardAction(player: Player, card: Card) throws {
+        try player.removeCardFromHand(card)
+        self.discards.append(card)
+    }
+    
+    func applyLayDownInitialBooksAction(player: Player, cards: [[Card]]) throws {
+        let books = try cards.map({ try Book(initialCards: $0) })
+        let pointsInBooks = books.reduce(0, { $0 + $1.pointValue })
+        
+        guard pointsInBooks >= self.round.rawValue else {
+            throw IllegalActionError.notEnoughPointsToLayDown
+        }
+        
+        for cardsInBook in cards {
+            try player.createBook(with: cardsInBook)
+        }
+    }
+    
+    func applyStartBookAction(player: Player, cards: [Card]) throws {
+        try player.createBook(with: cards)
+    }
+    
+    func applyAddCardFromHandToBookAction(player: Player, card: Card) throws {
+        try player.removeCardFromHand(card)
+        try player.addCardToBook(card)
     }
 }
