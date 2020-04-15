1fc68b5dc404e0fa5fbc1026ad69b3728bc0e0e1
Ben Oztalay <boztalay@gmail.com>
2020-04-14 21:51:27 -0700

    Rough cut of a login/signup screen and basic networking

---
+++ app/App/Network/Network.swift
@@ -0,0 +1,97 @@
+//
+//  Network.swift
+//  HandAndFoot
+//
+//  Created by Ben Oztalay on 4/14/20.
+//  Copyright © 2020 Ben Oztalay. All rights reserved.
+//
+
+import Foundation
+
+typealias NetworkResponseHandler = (Bool, Int?, JSONDictionary?) -> ()
+
+class Network {
+    
+    private static let baseUrl = URL(string: "https://handandfoot-boztalay.structure.sh")!
+    
+    static let shared = Network()
+    
+    var token: String?
+    
+    func sendLoginRequest(email: String, password: String, responseHandler: @escaping NetworkResponseHandler) {
+        self.sendRequest(
+            path: "/api/login",
+            payload: [
+                "email" : email,
+                "password" : password
+            ],
+            responseHandler: self.loginResponseHandler(originalResponseHandler: responseHandler)
+        )
+    }
+    
+    func sendSignUpRequest(name: String, email: String, password: String, responseHandler: @escaping NetworkResponseHandler) {
+        self.sendRequest(
+            path: "/api/signup",
+            payload: [
+                "name": name,
+                "email" : email,
+                "password" : password
+            ],
+            responseHandler: self.loginResponseHandler(originalResponseHandler: responseHandler)
+        )
+    }
+    
+    private func loginResponseHandler(originalResponseHandler: @escaping NetworkResponseHandler) -> NetworkResponseHandler {
+        return { success, httpStatusCode, response in
+            if success, let token = response?["token"] as? String {
+                self.token = token
+            }
+            
+            originalResponseHandler(success, httpStatusCode, response)
+        }
+    }
+    
+    private func sendRequest(path: String, payload: JSONDictionary, responseHandler: @escaping NetworkResponseHandler) {
+        let url = Network.baseUrl.appendingPathComponent(path)
+        var urlRequest = URLRequest(url: url)
+        
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpBody = try! JSONSerialization.data(withJSONObject: payload, options: [])
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let token = self.token {
+            urlRequest.addValue(token, forHTTPHeaderField: "X-App-Token")
+        }
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if error != nil {
+                responseHandler(false, nil, nil)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                responseHandler(false, nil, nil)
+                return
+            }
+            
+            guard let data = data else {
+                responseHandler(false, httpResponse.statusCode, nil)
+                return
+            }
+            
+            guard let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? JSONDictionary else {
+                responseHandler(false, httpResponse.statusCode, nil)
+                return
+            }
+            
+            guard let success = jsonResponse["success"] as? Bool else {
+                responseHandler(false, httpResponse.statusCode, jsonResponse)
+                return
+            }
+
+            responseHandler(success, httpResponse.statusCode, jsonResponse)
+        }
+
+        task.resume()
+    }
+}
---
+++ app/App/View Controllers/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  HandAndFoot
+//
+//  Created by Ben Oztalay on 4/14/20.
+//  Copyright © 2020 Ben Oztalay. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var logInEmailTextField: UITextField!
+    @IBOutlet weak var logInPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signUpNameTextField: UITextField!
+    @IBOutlet weak var signUpEmailTextField: UITextField!
+    @IBOutlet weak var signUpPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        guard let email = self.logInEmailTextField.text, let password = self.logInPasswordTextField.text else {
+            return
+        }
+        
+        Network.shared.sendLoginRequest(email: email, password: password) { success, httpStatusCode, response in
+            print("Success: \(success)")
+            print("Status Code: \(String(describing: httpStatusCode))")
+            print("Response: \(String(describing: response))")
+        }
+    }
+
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+
+    }
+}
