f55ed4c7df1a5fe0ff32d34afe87827caafa76f4
Ben Oztalay <boztalay@gmail.com>
2020-05-18 01:43:30 -0700

    Factor the scoreboard whatnot into ScoreBoardView, tweak GamePreviewView

--- app/App/Views/GamePreviewView.swift
+++ app/App/Views/GamePreviewView.swift
@@ -13,14 +13,14 @@ class GamePreviewView: UIView {
     var titleLabel: UILabel!
     var turnLabel: UILabel!
     var playButton: UIButton!
-    var scoreBoardStackView: UIStackView!
+    var scoreBoardView: ScoreBoardView!
 
     init() {
         super.init(frame: CGRect.zero)
         
         self.titleLabel = UILabel()
         self.turnLabel = UILabel()
-        self.scoreBoardStackView = UIStackView()
+        self.scoreBoardView = ScoreBoardView()
         self.playButton = UIButton(type: .system)
     }
     
@@ -32,88 +32,27 @@ class GamePreviewView: UIView {
         self.titleLabel.removeFromSuperview()
         self.addSubview(self.titleLabel)
         self.titleLabel.centerHorizontally(in: self)
-        self.titleLabel.pin(edge: .top, to: .top, of: self, with: 10)
+        self.titleLabel.pin(edge: .top, to: .top, of: self, with: 20)
+        self.titleLabel.font = UIFont.boldSystemFont(ofSize: 24.0)
         self.titleLabel.text = gameModel.title
         
         self.turnLabel.removeFromSuperview()
         self.addSubview(self.turnLabel)
         self.turnLabel.centerHorizontally(in: self)
         self.turnLabel.pin(edge: .top, to: .bottom, of: self.titleLabel, with: 10)
-        self.turnLabel.text = "\(gameModel.currentUser!.firstName!)'s turn"
-        
-        self.scoreBoardStackView.removeFromSuperview()
-        self.scoreBoardStackView = self.setUpScoreBoard(with: gameModel)
-        self.addSubview(self.scoreBoardStackView)
-        self.scoreBoardStackView.pinX(to: self, leading: 10, trailing: 10)
-        self.scoreBoardStackView.pin(edge: .top, to: .bottom, of: self.turnLabel, with: 10)
+        self.turnLabel.text = "It's \(gameModel.currentUser!.firstName!)'s turn"
         
         self.playButton.removeFromSuperview()
         self.addSubview(self.playButton)
         self.playButton.centerHorizontally(in: self)
-        self.playButton.pin(edge: .top, to: .bottom, of: self.scoreBoardStackView, with: 10)
-        self.playButton.pin(edge: .bottom, to: .bottom, of: self, with: -10)
+        self.playButton.pin(edge: .top, to: .bottom, of: self.turnLabel, with: 20)
         self.playButton.setTitle("Play!", for: .normal)
-    }
-    
-    func setUpScoreBoard(with gameModel: GameModel) -> UIStackView {
-        let scoreBoardStackView = UIStackView()
-        scoreBoardStackView.axis = .horizontal
-        scoreBoardStackView.distribution = .fillEqually
         
-        let headerColumnStackView = UIStackView()
-        headerColumnStackView.axis = .vertical
-        headerColumnStackView.distribution = .fillEqually
-        headerColumnStackView.addArrangedSubview(UIView())
-        
-        for round in Round.allCases {
-            let roundLabel = UILabel()
-            roundLabel.text = round.niceName
-            roundLabel.font = UIFont.boldSystemFont(ofSize: roundLabel.font.pointSize)
-            
-            let booksLabel = UILabel()
-            booksLabel.text = "    Books"
-            
-            let pointsLabel = UILabel()
-            pointsLabel.text = "    Points"
-
-            headerColumnStackView.addArrangedSubview(UIView())
-            headerColumnStackView.addArrangedSubview(roundLabel)
-            headerColumnStackView.addArrangedSubview(booksLabel)
-            headerColumnStackView.addArrangedSubview(pointsLabel)
-        }
-        
-        scoreBoardStackView.addArrangedSubview(headerColumnStackView)
-        
-        for player in gameModel.game!.players {
-            let playerColumnStackView = UIStackView()
-            playerColumnStackView.axis = .vertical
-            playerColumnStackView.distribution = .fillEqually
-            
-            let nameLabel = UILabel()
-            nameLabel.text = gameModel.user(with: player.name)!.firstName!
-            nameLabel.font = UIFont.boldSystemFont(ofSize: nameLabel.font.pointSize)
-            playerColumnStackView.addArrangedSubview(nameLabel)
-            
-            for round in Round.allCases {
-                let points = player.points[round]!
-                
-                let booksLabel = UILabel()
-                booksLabel.text = "\(points.inBooks)"
-                
-                let nonBooksPoints = points.inHand + points.inFoot + points.laidDown + points.forGoingOut
-                let pointsLabel = UILabel()
-                pointsLabel.text = "\(nonBooksPoints)"
-
-                playerColumnStackView.addArrangedSubview(UIView())
-                playerColumnStackView.addArrangedSubview(UIView())
-                playerColumnStackView.addArrangedSubview(booksLabel)
-                playerColumnStackView.addArrangedSubview(pointsLabel)
-            }
-            
-            scoreBoardStackView.addArrangedSubview(playerColumnStackView)
-        }
-        
-        return scoreBoardStackView
+        self.scoreBoardView.removeFromSuperview()
+        self.addSubview(self.scoreBoardView)
+        self.scoreBoardView.pinX(to: self, leading: 40, trailing: -40)
+        self.scoreBoardView.pin(edge: .top, to: .bottom, of: self.playButton, with: 50)
+        self.scoreBoardView.update(with: gameModel)
     }
     
     required init?(coder: NSCoder) {
---
+++ app/App/Views/ScoreBoardView.swift
@@ -0,0 +1,79 @@
+//
+//  ScoreBoardView.swift
+//  HandAndFoot
+//
+//  Created by Ben Oztalay on 5/18/20.
+//  Copyright © 2020 Ben Oztalay. All rights reserved.
+//
+
+import UIKit
+
+class ScoreBoardView: UIView {
+    
+    var stackView: UIStackView!
+
+    func update(with gameModel: GameModel) {
+        if let stackView = self.stackView {
+            stackView.removeFromSuperview()
+        }
+        
+        self.stackView = UIStackView()
+        self.addSubview(self.stackView)
+        self.stackView.pin(to: self)
+        self.stackView.axis = .horizontal
+        self.stackView.distribution = .fillEqually
+        
+        let headerColumnStackView = UIStackView()
+        headerColumnStackView.axis = .vertical
+        headerColumnStackView.distribution = .fillEqually
+        headerColumnStackView.addArrangedSubview(UIView())
+        
+        for round in Round.allCases {
+            let roundLabel = UILabel()
+            roundLabel.text = round.niceName
+            roundLabel.font = UIFont.boldSystemFont(ofSize: roundLabel.font.pointSize)
+            
+            let booksLabel = UILabel()
+            booksLabel.text = "    Books"
+            
+            let pointsLabel = UILabel()
+            pointsLabel.text = "    Points"
+
+            headerColumnStackView.addArrangedSubview(UIView())
+            headerColumnStackView.addArrangedSubview(roundLabel)
+            headerColumnStackView.addArrangedSubview(booksLabel)
+            headerColumnStackView.addArrangedSubview(pointsLabel)
+        }
+        
+        self.stackView.addArrangedSubview(headerColumnStackView)
+        
+        for player in gameModel.game!.players {
+            let playerColumnStackView = UIStackView()
+            playerColumnStackView.axis = .vertical
+            playerColumnStackView.distribution = .fillEqually
+            
+            let nameLabel = UILabel()
+            nameLabel.text = gameModel.user(with: player.name)!.firstName!
+            nameLabel.font = UIFont.boldSystemFont(ofSize: nameLabel.font.pointSize)
+            playerColumnStackView.addArrangedSubview(nameLabel)
+            
+            for round in Round.allCases {
+                let points = player.points[round]!
+                
+                let booksLabel = UILabel()
+                booksLabel.text = "\(points.inBooks)"
+                
+                let nonBooksPoints = points.inHand + points.inFoot + points.laidDown + points.forGoingOut
+                let pointsLabel = UILabel()
+                pointsLabel.text = "\(nonBooksPoints)"
+
+                playerColumnStackView.addArrangedSubview(UIView())
+                playerColumnStackView.addArrangedSubview(UIView())
+                playerColumnStackView.addArrangedSubview(booksLabel)
+                playerColumnStackView.addArrangedSubview(pointsLabel)
+            }
+            
+            self.stackView.addArrangedSubview(playerColumnStackView)
+        }
+    }
+}
